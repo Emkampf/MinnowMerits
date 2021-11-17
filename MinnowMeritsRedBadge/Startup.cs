e5950e46b3625f6d2d1d14dee2521317c2905374
Emily Kampf <emilykampf@gmail.com>
2021-11-17 13:39:30 -0600

    Add project files.

---
+++ MinnowMeritsRedBadge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MinnowMeritsRedBadge.Startup))]
+namespace MinnowMeritsRedBadge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
